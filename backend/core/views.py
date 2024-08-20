80b837611979c218bf850ea9726e76500bfeb216
Roman Volodin <roman.volodin@gmail.com>
2024-08-20 13:31:19 +0300

    Добавил полей в форму загрузки шотов из Экселя

--- backend/core/views.py
+++ backend/core/views.py
@@ -16,19 +16,57 @@ def read_xlsx(request):
         start_row = form.cleaned_data["start_row"]
         end_row = form.cleaned_data["end_row"]
         shot_name_column = form.cleaned_data["shot_name_column"]
+        rec_timecode_column = form.cleaned_data["rec_timecode_column"]
+        src_name_column = form.cleaned_data["source_name_column"]
+        src_start_tc_column = form.cleaned_data["source_start_timecode_column"]
+        src_end_tc_column = form.cleaned_data["source_end_timecode_column"]
+        pixel_aspect_column = form.cleaned_data["pixel_aspect_column"]
+        retime_speed_column = form.cleaned_data["retime_speed_column"]
+        scene_column = form.cleaned_data["scene_column"]
 
         new_shots = []
 
         wb = load_workbook(request.FILES["xlsx_file"])
         active_sheet = wb.active
         for row_number in range(start_row, end_row + 1):
-            new_shots.append(
-                Shot(
-                    name=active_sheet[f"{shot_name_column}{row_number}"].value,
-                    group=shot_group,
-                    created_by=created_by,
-                )
-            )
+            fields = {
+                "group": shot_group,
+                "created_by": created_by,
+            }
+
+            name = active_sheet[f"{shot_name_column}{row_number}"].value
+            fields["name"] = name
+
+            rec_timecode = active_sheet[f"{rec_timecode_column}{row_number}"].value
+            if rec_timecode is not None:
+                fields["rec_timecode"] = rec_timecode
+
+            source_name = active_sheet[f"{src_name_column}{row_number}"].value
+            if source_name is not None:
+                fields["source_name"] = source_name
+
+            source_start_timecode = active_sheet[f"{src_start_tc_column}{row_number}"].value
+            if source_start_timecode is not None:
+                fields["source_start_timecode"] = source_start_timecode
+
+            source_end_timecode = active_sheet[f"{src_end_tc_column}{row_number}"].value
+            if source_end_timecode is not None:
+                fields["source_end_timecode"] = source_end_timecode
+
+            pixel_aspect = active_sheet[f"{pixel_aspect_column}{row_number}"].value
+            if pixel_aspect is not None:
+                fields["pixel_aspect"] = pixel_aspect if isinstance(pixel_aspect, float) else 1
+
+            retime_speed = active_sheet[f"{retime_speed_column}{row_number}"].value
+            fields["retime_speed"] = retime_speed
+            if retime_speed is None:
+                fields["retime_speed"] = 100
+
+            scene = active_sheet[f"{scene_column}{row_number}"].value
+            if scene is not None:
+                fields["scene"] = scene
+
+            new_shots.append(Shot(**fields))
         Shot.objects.bulk_create(new_shots)
         return HttpResponse(f"{len(new_shots)} shot(s) created.")
     else:
