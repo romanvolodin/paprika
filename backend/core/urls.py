550238afd7689b260bf6c5feb04f0aff1cd7f4ae
Roman Volodin <roman.volodin@gmail.com>
2025-02-17 18:13:17 +0300

    Сделал доступ к страницам Джанго через префикс dj/

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,6 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
@@ -19,42 +14,42 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/me/", views.CurrentUserView.as_view(), name="current_user"),
     path("api/", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="drf")),
+    path("dj/api-auth/", include("rest_framework.urls", namespace="drf")),
     #
-    path("shots/from_xlsx/", views.read_xlsx),
-    path("shots/upload_previews/", views.save_multiple_uploaded_shot_previews),
+    path("dj/shots/from_xlsx/", views.read_xlsx),
+    path("dj/shots/upload_previews/", views.save_multiple_uploaded_shot_previews),
     path(
-        "<str:project_code>/groups/",
+        "dj/<str:project_code>/groups/",
         views.shot_group_list,
         name="shot_groups_list",
     ),
     path(
-        "<str:project_code>/groups/<int:shot_group_id>",
+        "dj/<str:project_code>/groups/<int:shot_group_id>",
         views.shot_group_details,
         name="shot_group_details",
     ),
     path(
-        "<str:project_code>/tasks/",
+        "dj/<str:project_code>/tasks/",
         views.task_list,
         name="task_list",
     ),
     path(
-        "<str:project_code>/tasks/<int:task_id>",
+        "dj/<str:project_code>/tasks/<int:task_id>",
         views.task_details,
         name="task_details",
     ),
     path(
-        "",
+        "dj/",
         views.project_list,
         name="project_list",
     ),
     path(
-        "<str:project_code>",
+        "dj/<str:project_code>",
         views.project_details,
         name="project_details",
     ),
     path(
-        "versions/upload/",
+        "dj/versions/upload/",
         views.save_multiple_uploaded_versions,
         name="versions_upload",
     ),
