ae17930949dd7ed2aa6205123cc261aabf22c11d
Roman Volodin <roman.volodin@gmail.com>
2025-05-11 16:24:46 +0300

    front: Добавил редирект на форму логина если сервер отвечате, что токен невалидный

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,8 +1,19 @@
-import axios from 'axios'
+import router from '@/router';
+import axios from 'axios';
 
 const instance = axios.create({
   timeout: 10000,
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.data?.code === 'token_not_valid') {
+      router.push('/login')
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default instance
