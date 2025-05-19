9bf425fa62a62d4e157cfcf04e4a422107835003
Roman Volodin <roman.volodin@gmail.com>
2025-05-20 00:23:19 +0300

    api: serializers.py -  Отсортировал импорты

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import Group
 from rest_framework import serializers
 
-from core.models import Project, Shot, ShotGroup, ShotTask, Status, Task, Version, ChatMessage
+from core.models import ChatMessage, Project, Shot, ShotGroup, ShotTask, Status, Task, Version
 from users.models import User
 
 
