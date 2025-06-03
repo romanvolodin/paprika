54054c9df2985b24428b41174fd65dcabc15ccac
Roman Volodin <roman.volodin@gmail.com>
2025-06-03 07:58:35 +0300

    back: Добавил настройки Simple JWT

--- backend/paprika/settings.py
+++ backend/paprika/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 
 from environs import Env
@@ -122,4 +123,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=12),
 }
