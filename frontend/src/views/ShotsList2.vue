c1437f5e8c4cabfdea43075adc226e897e903b41
Roman Volodin <roman.volodin@gmail.com>
2026-04-11 12:13:51 +0300

    Исправил пустое состояние
    Иначе при добавлении пустых групп в проект отображалась панель фильтров и не показывались кнопки создания шотов

--- frontend/src/views/ShotsList2.vue
+++ frontend/src/views/ShotsList2.vue
@@ -75,6 +75,10 @@ const assignees = computed(() => {
   return list
 })
 
+const totalShots = computed(() => {
+  return _groups.value.reduce((sum, group) => sum + group.shots.length, 0)
+})
+
 const filteredGroups = computed(() => {
   // Фильтрация по статусу
   const filterByStatus = (shot) => {
@@ -127,7 +131,7 @@ const filteredGroups = computed(() => {
   </div>
 
   <div v-else class="shots-list">
-    <div v-if="_groups.length === 0" class="empty-state">
+    <div v-if="totalShots === 0" class="empty-state">
       <div class="empty-state-illustration">
         <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
           <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
